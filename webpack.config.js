cadfa74b2bc6f168e919274b0a7b7b39570c4d0c
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-04-04 23:54:23 +0430

    Initial files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './src/index.jsx'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].main.js',
+    clean: true,
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Change Title in Webpack Config',
+      favicon: '',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+};
